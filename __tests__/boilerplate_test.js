6f547b24c7fa502018aedc355bbbffb511702815
Dinakaran S <dinakaran@nobel-systems.com>
2019-11-14 14:38:22 +0530

    Updates

--- __tests__/boilerplate_test.js
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-
-describe('Boilerplate', function () {
-  it('should do boilerplate things', function () {
-    // TODO: test something now
-    expect(true).to.equal(true);
-  });
-});
